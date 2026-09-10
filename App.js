dd71e0d930040841eb1e0e45d2700e6f98e1b1c8
reyhan <reyhanhussain@hotmail.com>
2026-09-10 21:55:55 +0530

    learing tool calling

--- App.js
+++ App.js
@@ -1,57 +1,6 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import Form from "./components/Form";
 import Table from "./components/Table";
-
-const applications = [
-  {
-    id: 1,
-    company: "Google",
-    role: "Software Engineer",
-    status: "Applied",
-    date: "08/09/2026",
-    ref: "LinkedIn",
-  },
-  {
-    id: 2,
-    company: "Microsoft",
-    role: "Frontend Developer",
-    status: "Interview",
-    date: "07/09/2026",
-    ref: "Referral",
-  },
-  {
-    id: 3,
-    company: "Amazon",
-    role: "SDE-1",
-    status: "No Confirmation",
-    date: "05/09/2026",
-    ref: "Company Website",
-  },
-  {
-    id: 4,
-    company: "Infosys",
-    role: "Systems Engineer",
-    status: "Rejected",
-    date: "03/09/2026",
-    ref: "Campus",
-  },
-  {
-    id: 5,
-    company: "Deloitte",
-    role: "Analyst",
-    status: "Applied",
-    date: "01/09/2026",
-    ref: "LinkedIn",
-  },
-  {
-    id: 6,
-    company: "Accenture",
-    role: "Associate Software Engineer",
-    status: "Interview",
-    date: "28/08/2026",
-    ref: "College Placement",
-  },
-];
 
 function App() {
   const [form, setForm] = useState({
@@ -62,7 +11,96 @@ function App() {
     date: "",
     ref: "",
   });
+    const [btn, setBtn] = useState(false);
+ 
+  function sayHello({ name }) {
+    console.log(`Hello, ${name}!`);
+    return `Successfully greeted ${name}.`;
+  }
 
+  // 2. The definition the model reads
+  const tools = [
+    {
+      type: "function",
+      function: {
+        name: "sayHello",
+        description: "Greets a person by name.",
+        parameters: {
+          type: "object",
+          properties: {
+            name: {
+              type: "string",
+              description: "The name of the person to greet",
+            },
+          },
+          required: ["name"],
+        },
+      },
+    },
+  ];
+
+  const toolRegistry = {
+    sayHello:sayHello
+  };
+
+  async function fetchData() {
+
+    try{
+    let response = await fetch("https://openrouter.ai/api/v1/chat/completions", {
+      method: "POST",
+      headers: {
+        "Authorization": `Bearer ${key}`,
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify({
+        "model": "liquid/lfm-2.5-2.6b:free",
+        "messages": [
+          {
+            "role": "user",
+            // "content": "Please say hello to Reyhan using tools provided "
+            "content": "Please Say hello to Reyhan 3 times "
+          }
+        ],
+        "tools": tools,
+        "reasoning": { "effort": "medium" }
+      })
+    });
+
+
+
+    const result = await response.json();
+    response = result.choices[0].message;
+    console.log('orrginal rest', result)
+    console.log('orrginal responseObjn', response)
+
+    if(response.tool_calls){
+      console.log('going inside tool call func')
+      for(const toolCall of response.tool_calls){
+        const args = JSON.parse(toolCall.function.arguments);
+        const fnName = toolCall.function.name;
+        console.log('arg and fnName are:', args, fnName);
+
+        if(toolRegistry[fnName]){
+          const op = toolRegistry[fnName](args);
+          console.log('func returned val: ', op)
+        }
+      }
+    }
+    else{
+      console.log('model resp', response.content)
+    }
+
+  }
+  catch(e){
+    console.log(e)
+  }
+  }
+  // const toolRegistry = [toolRegistry];
+
+  useEffect(()=>{
+    fetchData();
+
+  }, [btn])
   const [list, setList] = useState([]);
   console.log("list", list);
 
@@ -130,15 +168,15 @@ function App() {
 
 
 
-    Object.keys(errorValues).forEach((key)=>{
-        if(form[key].trim() == ''){
-          errorTemp['length'] = errorTemp['length']+1;
-          errorTemp[key] = errorValues[key];
-        }
+    Object.keys(errorValues).forEach((key) => {
+      if (form[key].trim() == '') {
+        errorTemp['length'] = errorTemp['length'] + 1;
+        errorTemp[key] = errorValues[key];
+      }
     })
     setError(errorTemp)
     console.log('temp error we designed is: ', errorTemp);
-    if(errorTemp.length > 0){
+    if (errorTemp.length > 0) {
       return true;
     }
     return false;
@@ -157,6 +195,7 @@ function App() {
         setList={setList}
       />
       <Table AllInfos={list} />
+      <button onClick={()=>setBtn(true)}>click</button>
     </div>
   );
 }
